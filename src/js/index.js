fe3b919d3bdc6cb82b09d2d4bb90d934f0844c2e
Tomasz Rosko <tomasz.rosko@gmail.com>
2023-02-27 14:29:43 +0100

    bug-fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,14 @@
 const menuItems = document.querySelectorAll(".menuItem")
 const activeShow = document.querySelector('.offcanvas-end')
 const backdropShow = document.getElementsByClassName("offcanvas-backdrop")
+const bodyStyle = document.querySelector('body')
 
 const handle = () => {
     if (activeShow.classList.contains("show") && backdropShow[0].classList.contains("show"))  {
         activeShow.classList.remove("show")
         backdropShow[0].classList.remove("show")
+        bodyStyle.style.overflow = 'auto'
+
     }
 }
 
